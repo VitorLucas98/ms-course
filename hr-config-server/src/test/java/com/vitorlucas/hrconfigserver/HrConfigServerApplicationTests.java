18554a54f83eb766c0fe75178195f6d8b4540c37
Vitor Lucas <vitorluc.2013@gmail.com>
2021-04-21 16:35:44 -0300

    Project hr-config-server created!

---
+++ hr-config-server/src/test/java/com/vitorlucas/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorlucas.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
