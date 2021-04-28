86c013785a5f8c2da2a0c09d8e9d0a1c301d125e
Vitor Lucas <vitorluc.2013@gmail.com>
2021-04-27 22:07:05 -0300

    hr-user created

---
+++ hr-user/src/test/java/com/vitorlucas/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorlucas.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
